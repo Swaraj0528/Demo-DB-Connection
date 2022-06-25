23ec48adca75ded8c70a578792b620c4135d2ee1
Swaraj Varma <swaraj.varma@outlook.com>
2022-06-25 14:09:49 +0530

    this is initial commit

---
+++ src/main/java/com/DBConn/DemoDbConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.DBConn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDbConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDbConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DBConn/DemoDbConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBConn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDbConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
